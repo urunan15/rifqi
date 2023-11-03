2508b01248d19dd93769ac709cf0fcabb2dc8cc1
rifqi <rifqiakbar551@gmail.com>
2023-11-03 08:57:19 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const navbarNav =document.querySelector('.navbar-nav');
+// Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
